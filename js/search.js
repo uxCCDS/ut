fefe973443bb244d51214be215a894fa23bef517
Arthus <531151995@qq.com>
2019-01-04 16:05:19 +0800

    add

--- js/search.js
+++ js/search.js
@@ -1,8 +1,36 @@
 (function(){
 
+var Search = function(){
+	this.Con = $('#searchCon');
+	this.Ipt = $('#iptSearch');
+	this.init();
+};
+Search.prototype={
+	init:function(){
+		var me = this;
+		this.Ipt.bind('focus',function(){
+			me.focus();
+		});
+		this.Ipt.bind('blur',function(){
+			me.blur();
+		});
+	},
+	isIptEmpty:function(){
+		return this.Ipt.val().replace(/\r\n\s\t/g,'')==='';
+	},
+	focus:function(){
+		this.Ipt.addClass('onIpt');
+	},
+	blur:function(){
+		if(this.isIptEmpty()){
+			this.Ipt.removeClass('onIpt');
+		}
+	}
+};
+
 $(function(){
 
-	var ipt = $('#iptSearch');
+	new Search();
 
 });
 
